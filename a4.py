4366c974f3b1a90571634f3eade7b2d187e8c753
Sukru Birim <sukru.birim@gmail.com>
2025-02-09 17:36:12 +0300

    09.02(2)

---
+++ a4.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x_coords=[0,1,2,3,4,5]
+y_coords=[4,5,6,3,2,9]
+
+plt.plot(x_coords,y_coords)
+plt.title("Sample Data")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.grid(True)
+plt.show()
