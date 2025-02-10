423c2c291087e39a7806a38e50ae9133355a6175
Sukru Birim <sukru.birim@gmail.com>
2025-02-10 09:33:46 +0300

    3

--- a4.py
+++ a4.py
@@ -1,11 +1,35 @@
 import matplotlib.pyplot as plt
+    
+def line_graph():
+    x_coords = [0, 1, 2, 3, 4] 
+    y_coords = [0, 3, 1, 5, 2]
 
-x_coords=[0,1,2,3,4,5]
-y_coords=[4,5,6,3,2,9]
+    plt.plot(x_coords,y_coords,marker="o")
+    plt.title("Sales by Year")
+    plt.xlabel("Year")
+    plt.ylabel("Sales")
 
-plt.plot(x_coords,y_coords)
-plt.title("Sample Data")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-plt.grid(True)
-plt.show()
+    plt.xticks([0,1,2,3,4],list(range(2016,2021)))
+    plt.yticks([0,1,2,3,4,5],["$0m","$1m","$2m","$3m","$4m","$5m"])
+
+    plt.grid(True)
+    plt.show()
+
+def bar_chart():
+    left_edges=[0,10,20,30,40]
+    heights=[100,200,300,400,500]
+    
+    plt.xlim(xmin=-5,xmax=45)
+    plt.ylim(ymin=0,ymax=500)
+    
+    plt.title("Sales by Year")
+    plt.xlabel("Year")
+    plt.ylabel("Sales")
+    plt.xticks([0,10,20,30,40],list(range(2016,2021)))
+    plt.yticks([0,100,200,300,400,500],["$0m","$1m","$2m","$3m","$4m","$5m"])
+    
+    bar_width=10
+    plt.bar(left_edges, heights, bar_width,color=('r',"m","c","y","b"))
+    plt.show()
+bar_chart()
+    
